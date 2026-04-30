5962fc852f3d3fe53d1dc03f887be9e30573ffe2
Anxiu0101 <anxiu.fyc@foxmail.com>
2026-04-30 06:56:01 +0100

    feat: code-block copy button.

--- components/mdx/mdx-content.tsx
+++ components/mdx/mdx-content.tsx
@@ -2,10 +2,11 @@ import * as runtime from 'react/jsx-runtime'
 import React, {ComponentPropsWithoutRef} from "react";
 import { ImageWithFallback } from "@/components/mdx/image-with-fallback";
 import { TravelGallery } from "@/components/mdx/travel-gallery";
+import { CodeBlock } from "@/components/mdx/code-block";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
-// https://github.com/Stryke-AI/shadcn-ui/blob/main/apps/v4/mdx-components.tsx
+// 风格参考 https://github.com/Stryke-AI/shadcn-ui/blob/main/apps/v4/mdx-components.tsx
 const sharedComponents = {
     TravelGallery,
     // Add your global components here
@@ -15,49 +16,13 @@ const sharedComponents = {
     h3: (props: React.ComponentProps<'h3'>) => (
         <h3 className="scroll-m-20 text-2xl p-1 font-semibold tracking-tight [&+p]:mt-4! *:[code]:text-2xl" {...props}/>
     ),
-    // h3: ({ className, ...props }: React.ComponentProps<"h3">) => (
-    //     <h3
-    //         className={cn(
-    //             "mt-12 scroll-m-28 font-heading text-lg font-medium tracking-tight [&+p]:mt-4! *:[code]:text-xl",
-    //             className
-    //         )}
-    //         {...props}
-    //     />
-    // ),
-    // p: (props: ComponentPropsWithoutRef<'p'>) => (
-    //     <p className="leading-7 not-first:mt-6" {...props}/>
-    // ),
     p: ({ className, ...props }: React.ComponentProps<"p">) => (
         <p
             className={cn("leading-relaxed not-first:mt-3 mb-2", className)}
             {...props}
         />
     ),
-    pre: ({ className, ...props }: React.ComponentProps<"pre">) => (
-        <pre
-            className={cn("not-first:mt-3 mb-2 overflow-auto rounded-lg py-1", className)}
-            {...props}
-        />
-    ),
-    // pre: (props: ComponentPropsWithoutRef<'pre'>) => (
-    //     <pre
-    //         className="p-4 rounded-lg border overflow-x-auto bg-[var(--shiki-light-bg)] dark:bg-[var(--shiki-dark-bg)]"
-    //         {...props}
-    //     />
-    // ),
-    // pre: ({ className, children, ...props }: React.ComponentProps<"pre">) => {
-    //     return (
-    //         <pre
-    //             // className={cn(
-    //             //     "no-scrollbar min-w-0 overflow-x-auto overflow-y-auto overscroll-x-contain overscroll-y-auto px-4 py-3.5 outline-none has-data-highlighted-line:px-0 has-data-line-numbers:px-0 has-data-[slot=tabs]:p-0",
-    //             //     className
-    //             // )}
-    //             {...props}
-    //         >
-    //     {children}
-    //   </pre>
-    //     )
-    // },
+    pre: (props: React.ComponentProps<"pre">) => <CodeBlock {...props} />,
     code: ({ className, ...props }: ComponentPropsWithoutRef<'code'>) => {
         // judge code block or not(By rehype-pretty-code injection attribute 'data-language')
         const isCodeBlock = props.hasOwnProperty('data-language');
@@ -123,6 +88,7 @@ export const MDXContent = ({ code, components }: MDXProps) => {
     return (
         <div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
             <React.Suspense fallback={<div>loading...</div>}>
+                {/* eslint-disable-next-line react-hooks/static-components */}
                 <MDXComponent components={{ ...sharedComponents, ...components }} />
             </React.Suspense>
         </div>
